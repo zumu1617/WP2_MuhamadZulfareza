9702b01d8d44c98e649ca17cf65fafd8c9818ab7
zumu1617 <kucing0318@gmail.com>
2023-09-26 19:50:22 +0700

    pertemuan2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function Index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhamad Zulfareza saya tinggal di daerah Kemyoran, Olah raga yang saya sukai adalah bulutangkis";
+    }
+}
