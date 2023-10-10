55290d9405d2c8fb93e3c513e04ca72ff9672d01
zumu1617 <kucing0318@gmail.com>
2023-10-10 20:36:18 +0700

    Pertemuan4

---
+++ application/views/v_header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1>RentalBuku.net</h1>
+                <h3>Membuat Template Sederhana dengan CodeIgniter</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
+                    <li><a href="<?php echo base_url().'index.php/web' ?>">About</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+    
+    </header>
